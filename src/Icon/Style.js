300821172a272cae515a4111246f51b6fc2b1023
陈翠卓 <chuizhuochen@angejia.com>
2016-01-21 21:00:09 +0800

    add icon default style

--- src/Icon/Style.js
+++ src/Icon/Style.js
@@ -1,5 +1,13 @@
 let Style = {
-    iconfont: {}
+    iconfont: {
+        fontFamily:"iconfont",
+        fontSize:'16px',
+        color: '#B2B2B2',
+        fontStyle:'normal',
+        WebkitFontSmoothing: 'antialiased',
+        WebkitTextStrokeWidth: '0.2px',
+        MozOsxFontSmoothing: 'grayscale',
+    }
 };
 
 module.exports = Style
