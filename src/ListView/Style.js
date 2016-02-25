4b916249e761705149b130b62ccae5339ac8de3a
储珍珍 <zhenzhenchu@angejia.com>
2016-02-25 16:36:22 +0800

    list border

--- src/ListView/Style.js
+++ src/ListView/Style.js
@@ -2,18 +2,19 @@ let style = {
     list: {
         position: 'relative',
         backgroundColor: 'white',
-        marginBottom: 34
+        marginBottom: 34,
+        overflow: 'hidden'
     },
     borderTop: {
         width: '100%',
         height: 1,
-        backgroundColor: '#ddd',
+        backgroundColor: '#d9d9d9',
         content: " ",
         left: 0,
         position: 'absolute'
     },
     borderBottom: {
-        bottom: -1
+        bottom: 0
     }
 };
 
